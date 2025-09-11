91f4a030b05e84cab146bbf9675ad53667998caf
Rémi Cérès <remi.ceres@inria.fr>
2025-09-11 11:54:51 +0200

    Add Javadoc to GraphWrapper and RDFLoaderWrapper classes

--- src/main/java/fr/inria/corese/command/utils/coreseCoreWrapper/GraphWrapper.java
+++ src/main/java/fr/inria/corese/command/utils/coreseCoreWrapper/GraphWrapper.java
@@ -2,12 +2,27 @@ package fr.inria.corese.command.utils.coreseCoreWrapper;
 
 import fr.inria.corese.core.Graph;
 
+/**
+ * A wrapper class for Corese Graph operations.
+ * This class provides static methods to create and manipulate Graph instances.
+ */
 public class GraphWrapper {
 
+    /**
+     * Creates a new empty Graph instance.
+     *
+     * @return a new Graph object
+     */
     public static Graph createGraph() {
         return Graph.create();
     }
 
+    /**
+     * Merges the contents of the resultGraphUrl into the given graph.
+     *
+     * @param graph the graph to merge into
+     * @param resultGraphUrl the graph whose contents are to be merged
+     */
     public static void mergeGraph(Graph graph, Graph resultGraphUrl) {
         graph.merge(resultGraphUrl);
     }    
--- src/main/java/fr/inria/corese/command/utils/coreseCoreWrapper/RDFLoaderWrapper.java
+++ src/main/java/fr/inria/corese/command/utils/coreseCoreWrapper/RDFLoaderWrapper.java
@@ -13,14 +13,34 @@ import fr.inria.corese.core.load.LoadFormat;
  */
 public class RDFLoaderWrapper {
 
+    /**
+     * Creates a Load instance for the given graph.
+     *
+     * @param graph the graph to load data into
+     * @return a Load instance
+     */
     public static Load graphLoader(Graph graph) {
         return Load.create(graph);
     }
 
+    /**
+     * Determines the load format from the input string.
+     *
+     * @param input the input string representing the format
+     * @return the corresponding Loader.format
+     */
     public static Loader.format getLoadFormat(String input) {
         return LoadFormat.getFormat(input);
     }
 
+    /**
+     * Parses RDF data from an InputStream into the given Load instance using the specified format.
+     *
+     * @param loader      the Load instance to use for parsing
+     * @param inputStream the InputStream containing RDF data
+     * @param inputFormat the format of the RDF data
+     * @throws LoadException if an error occurs during parsing
+     */
     public static void parse(Load loader, InputStream inputStream, Load.format inputFormat) throws LoadException {
         loader.parse(inputStream, inputFormat);
     }
